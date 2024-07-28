505e2c53e90bab44761e679646d416f1b0f3c491
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2024-07-28 18:37:49 -0300

    Update main.py

--- t2/main.py
+++ t2/main.py
@@ -1,50 +1,7 @@
 import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from tabulate import tabulate
-
-
-def plota_grafo(fxy, hist):
-    # Generate x and y values
-    x = np.linspace(-5, 5, 100)
-    y = np.linspace(-5, 5, 100)
-
-    # Create a meshgrid for x and y values
-    X, Y = np.meshgrid(x, y)
-    Z = fxy(X, Y)
-
-    # Create a figure with two subplots: one for the 3D plot and one for the contour plot
-    fig = plt.figure(figsize=(14, 6))
-
-    # pontos x, y e z
-    pontos_x = hist['xn']
-    pontos_y = hist['yn']
-    pontos_z = hist['f(xn, yn)']
-
-    # 3D surface plot
-    ax1 = fig.add_subplot(121, projection='3d')
-    ax1.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5)
-    ax1.set_xlabel('X axis')
-    ax1.set_ylabel('Y axis')
-    ax1.set_zlabel('Z axis')
-    ax1.set_title('3D Surface Plot')
-    # Create a scatter plot in 3D with red points
-    ax1.scatter(pontos_x, pontos_y, pontos_z, color='red')
-
-    # Contour plot
-    ax2 = fig.add_subplot(122)
-    contour = ax2.contour(X, Y, Z, cmap='viridis')
-    ax2.set_xlabel('X axis')
-    ax2.set_ylabel('Y axis')
-    ax2.set_title('Contour Plot')
-    fig.colorbar(contour, ax=ax2, shrink=0.8, aspect=5) # Add a colorbar to the contour plot
-    ax2.scatter(pontos_x, pontos_y, color='red')
-
-    # Show the plots
-    plt.tight_layout()
-    plt.show()
-
 
 
 def main():
@@ -101,11 +58,52 @@ def main():
         yn = yn1
         n_iteracoes += 1
     
-    minimo_local = [round(xn, 1), round(yn, 1)]
+    # mostra os resultados
+    minimo_local = [round(xn, 3), round(yn, 3)]
     print(tabulate(historico, headers='keys', tablefmt='grid'))
-    print(f'ponto de mínimo local: {minimo_local}')
-
+    print(f'\n>>>ponto de mínimo local: {minimo_local}')
     plota_grafo(fxy, historico)
+
+
+def plota_grafo(fxy, hist):
+    # Generate x and y values
+    x = np.linspace(-5, 5, 100)
+    y = np.linspace(-5, 5, 100)
+
+    # Create a meshgrid for x and y values
+    X, Y = np.meshgrid(x, y)
+    Z = fxy(X, Y)
+
+    # Create a figure with two subplots: one for the 3D plot and one for the contour plot
+    fig = plt.figure(figsize=(14, 6))
+
+    # pontos x, y e z
+    pontos_x = hist['xn']
+    pontos_y = hist['yn']
+    pontos_z = hist['f(xn, yn)']
+
+    # 3D surface plot
+    ax1 = fig.add_subplot(121, projection='3d')
+    ax1.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5)
+    ax1.set_xlabel('X axis')
+    ax1.set_ylabel('Y axis')
+    ax1.set_zlabel('Z axis')
+    ax1.set_title('3D Surface Plot')
+    # Create a scatter plot in 3D with red points
+    ax1.scatter(pontos_x, pontos_y, pontos_z, color='red')
+
+    # Contour plot
+    ax2 = fig.add_subplot(122)
+    contour = ax2.contour(X, Y, Z, cmap='viridis')
+    ax2.set_xlabel('X axis')
+    ax2.set_ylabel('Y axis')
+    ax2.set_title('Contour Plot')
+    fig.colorbar(contour, ax=ax2, shrink=0.8, aspect=5) # Add a colorbar to the contour plot
+    ax2.scatter(pontos_x, pontos_y, color='red')
+
+    # Show the plots
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
